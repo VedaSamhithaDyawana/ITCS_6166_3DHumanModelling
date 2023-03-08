c3877b44e1d4e82411bf87ce66a6cfa04014d71d
VedaSamhithaDyawana <124637918+VedaSamhithaDyawana@users.noreply.github.com>
2023-03-07 20:52:10 -0500

    Add files via upload

---
+++ public/js/config.js
@@ -0,0 +1,5 @@
+turnConfig = {
+    iceServers: [{   urls: [ "stun:bn-turn1.xirsys.com" ]}, {   
+        username: "x5EDzAeZkSUe4zctxAKbiYi8hJAkEfpuND59d09sva4bIq3N_GlN76gm-ermhAloAAAAAGQHwhR2aWduZXNod2Fy",
+        credential: "df395c98-bd3b-11ed-8d5b-0242ac140004",   urls: [       "turn:bn-turn1.xirsys.com:80?transport=udp",       "turn:bn-turn1.xirsys.com:3478?transport=udp",       "turn:bn-turn1.xirsys.com:80?transport=tcp",       "turn:bn-turn1.xirsys.com:3478?transport=tcp",       "turns:bn-turn1.xirsys.com:443?transport=tcp",       "turns:bn-turn1.xirsys.com:5349?transport=tcp"   ]}]
+}
